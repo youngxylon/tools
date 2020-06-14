c110ad589c245713be30578d2f7a54e254a649df
youngxylon <xylonyoung@live.com>
2020-06-14 21:34:18 +0800

    update

--- wechat/weChatLogin.js
+++ wechat/weChatLogin.js
@@ -1,30 +1,31 @@
-import request from './request';
-import store from '@/store';
+import request from "./request";
 export default function login() {
-    //get the code and state
-    if (location.search.indexOf('code') != -1) {
-        let obj = {};
-        let arr = location.search.slice(1).split('&');
-        arr.forEach(val => {
-            let arr1 = val.split('=');
-            obj[arr1[0]] = arr1[1];
-        });
-        //wechat login
-        request({
-            url: '/wechat/login',
-            method: 'get',
-            params: obj,
-        }).then(res => {
-            //save the token
-            store.commit('$setToken', res.data);
-            location.reload()
-        });
-    } else {
-        //need the authorized
-        request({
-            url: '/wechat/generate-auth-url',
-            method: 'get',
-            params: { redirect_url: location.href },
-        }).then(res => (location.href = res.data));
-    }
+  //get the code and state
+  if (location.search.indexOf("code") != -1) {
+    let obj = {};
+    let arr = location.search.slice(1).split("&");
+    arr.forEach(val => {
+      let arr1 = val.split("=");
+      obj[arr1[0]] = arr1[1];
+    });
+    //wechat login
+    request({
+      url: "/wechat/official/api-login",
+      method: "get",
+      params: obj
+    }).then(res => {
+      //save the token
+      localStorage.setItem("token", res.data);
+      location.reload();
+    });
+  } else {
+    //need the authorized
+    request({
+      url: "/wechat/generate-auth-url",
+      method: "get",
+      params: { redirect_url: location.href }
+    }).then(res => {
+      location.replace(res.data);
+    });
+  }
 }
