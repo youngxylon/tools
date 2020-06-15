5ce36a46e50b7bdd148aac2f63c0cb10c4248e33
xylon <xylonyoung@live.com>
2020-06-15 17:40:44 +0800

    update

--- js/getQuery.js
+++ js/getQuery.js
@@ -1,31 +1,25 @@
 /**
  * get location query by name
- * @param {string} variable
+ * @param {string} name
  * @returns {*}
  */
-export default function getQuery(variable) {
-    const link = decodeURIComponent(window.location.href);
-    if (link.includes('?')) {
-        let query = link.split('?')[1];
-        let vars = query.split('&');
-        for (let i = 0; i < vars.length; i++) {
-            let pair = vars[i].split('=');
-            if (pair[0] == variable) {
-                return pair[1];
-            }
-        }
-    }
-    return false;
+export default function getQuery(name) {
+  let obj = searchToObject();
+  if (obj.hasOwnProperty(name)) {
+    return obj[name];
+  } else {
+    return;
+  }
 }
 
 function searchToObject() {
-    let pairs = window.location.search.substring(1).split('&'),
-        obj = {},
-        pair;
-    for (let i in pairs) {
-        if (pairs[i] === '') continue;
-        pair = pairs[i].split('=');
-        obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-    }
-    return obj;
+  let pairs = window.location.search.substring(1).split('&'),
+    obj = {},
+    pair;
+  for (let i in pairs) {
+    if (pairs[i] === '') continue;
+    pair = pairs[i].split('=');
+    obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+  }
+  return obj;
 }
