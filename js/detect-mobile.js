17f08496e4bf59698f57e3e34d95a0cdf5c6ddce
xylon <xylonyoung@live.com>
2020-06-16 17:44:20 +0800

    update

--- js/detect-mobile.js
+++ js/detect-mobile.js
@@ -2,7 +2,7 @@
  * Find out the device is mobile or not
  * @returns {boolean}
  */
-export default function isMobile() {
+export default function detectMobile() {
     const flag = navigator.userAgent.match(
         /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
     );
