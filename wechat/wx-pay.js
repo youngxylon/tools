69d6ee3a933e1c12413e38292711576892258feb
xylon <xylonyoung@live.com>
2020-06-24 18:38:39 +0800

    Update wx-pay.js

--- wechat/wx-pay.js
+++ wechat/wx-pay.js
@@ -1,12 +1,12 @@
 import wx from "weixin-js-sdk";
-// npm install weixin-js-sdk
+// npm install -s weixin-js-sdk 
 export default function wxPay(data) {
   //获取后台传入的数据
   let appId = data.appId;
   let timestamp = data.timestamp;
   let nonceStr = data.nonceStr;
   let signature = data.signature;
-  let package = data.package;
+  let packages = data.package;
   let paySign = data.paySign;
   let signType = data.signType;
   console.log("发起微信支付");
@@ -23,7 +23,7 @@ export default function wxPay(data) {
     wx.chooseWXPay({
       timestamp: timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
       nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
-      package: package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+      package: packages, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
       signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
       paySign: paySign, // 支付签名
       success: function(res) {
