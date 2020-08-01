8ab78319ede779bfac37ede9f7ba49c3f2d20f75
youngxylon <xylonyoung@live.com>
2020-08-01 22:42:20 +0800

    update

---
+++ ts/get-property.ts
@@ -0,0 +1,19 @@
+export default function getProperty(obj: object, key: any): any {
+  if(!obj){
+    return false
+  }
+
+  if (typeof key === 'string') {
+    return getProperty(obj, key.split('.'))
+  }
+
+  if(key.length === 1 && obj.hasOwnProperty(key[0])){
+    return obj[key[0]]
+  }
+
+  if(obj.hasOwnProperty(key[0])){
+    return getProperty(obj[key[0]],key.slice(1))
+  }
+
+  return false
+}
