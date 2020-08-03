4653a6161d3b3986ebffbca3ee2537d18efc8dc1
youngxylon <xylonyoung@live.com>
2020-08-03 23:26:00 +0800

    update

---
+++ console.js
@@ -0,0 +1,4 @@
+console.log(
+  '%c ',
+  'background: url(https://i0.hdslb.com/bfs/archive/e62b6b095ef38dfb742687f11e4b570dde420b5d.png) no-repeat center;padding-left:300px;padding-bottom: 80px;'
+)
