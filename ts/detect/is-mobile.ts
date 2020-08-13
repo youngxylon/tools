a48975521d67e143af039d3b0ad09e94d83d0871
xylon <xylonyoung@live.com>
2020-08-13 17:41:59 +0800

    update

--- ts/detect/is-mobile.ts
+++ ts/detect/is-mobile.ts
@@ -1,7 +1,7 @@
 //Determine if mobile device
 export function mobile(): boolean {
   const regex: RegExp = new RegExp(
-    /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/,
+    '/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/',
     'i'
   )
   return regex.test(navigator.userAgent)
