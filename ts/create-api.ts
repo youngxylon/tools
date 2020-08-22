58527e92df3217fe15ed6afbb763c76ad72f3f3f
xylon <xylonyoung@live.com>
2020-08-22 18:14:45 +0800

    update

--- ts/create-api.ts
+++ ts/create-api.ts
@@ -2,29 +2,36 @@ interface Api {
   [key: string]: any
 }
 
-let api: Api = {}
-
-function createApi(arr: Api[]): void {
+export default function createApi(arr: string[]): object {
+  let api: Api = {}
   arr.forEach(e => {
-    const args: string[] = e.args
-    e.type.forEach(i => {
-      api[e.name] = new Function(
-        'methods',
-        ...args,
-        'params',
-        `return request(methods, ${parseUrl(e.url)},${args},'params')`
-      )
-    })
+    let parseObj: object = parseUrl(e)
+    api[parseObj['name']] = new Function(
+      'method',
+      'params',
+      '...args',
+      `return request({url:'${parseObj['url']}', method:method ,params})`
+    )
   })
+  return api
 }
 
-function parseUrl(url: string): string {
-  const reg = new RegExp('{', 'i')
-  if (reg.test(url)) {
-    return url.replace(reg, '${')
+function parseUrl(url: string): object {
+  const urlArr = url.split('/')
+  if (urlArr.length > 0) {
+    let name: string = ''
+    urlArr.forEach(e => {
+      if (e) {
+        name += e.replace(e[0], e[0].toUpperCase())
+      }
+    })
+    const regex = /{/gi
+    const parsedUrl = url.replace(regex, '${')
+    return {
+      name: name.replace(name[0], name[0].toLowerCase()),
+      url: parsedUrl,
+    }
   } else {
-    return url
+    return {}
   }
 }
-
-export default api
--- ts/detect/is-mobile.ts
+++ ts/detect/is-mobile.ts
@@ -1,8 +1,5 @@
 //Determine if mobile device
 export function mobile(): boolean {
-  const regex: RegExp = new RegExp(
-    '/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/',
-    'i'
-  )
+  const regex: RegExp = /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
   return regex.test(navigator.userAgent)
 }
