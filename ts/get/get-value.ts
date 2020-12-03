205492753d0bb2de64570dd90865edeec3516050
xylon <xylonyoung@live.com>
2020-12-03 17:18:14 +0800

    Update get-value.ts

--- ts/get/get-value.ts
+++ ts/get/get-value.ts
@@ -7,11 +7,11 @@ export function getValue(obj: object | any[], key: string | string[]): string | 
     return getValue(obj, key.split('.'))
   }
 
-  if (key.length === 1 && obj.hasOwnProperty(key[0])) {
+  if (key.length === 1 && Object.prototype.hasOwnProperty.call(obj, key[0])) {
     return obj[key[0]]
   }
 
-  if (obj.hasOwnProperty(key[0])) {
+  if (Object.prototype.hasOwnProperty.call(obj, key[0])) {
     return getValue(obj[key[0]], key.slice(1))
   }
 }
