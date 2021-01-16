36add6c835485230900316170367e1e2c42656b7
xylon <xylonyoung@live.com>
2021-01-16 17:34:04 +0800

    update

--- js/get-value.js
+++ js/get-value.js
@@ -1,24 +1,29 @@
-function getValue(obj, key) {
-  if (!obj) {
+/**
+ * get the value in Object or Array
+ * key include "." to separate
+ * @param {Object | Array} arg
+ * @param {String} key
+ * @returns {String | null}
+ */
+function getValue(arg, key) {
+  if (!arg) {
     return null
   }
 
   if (typeof key === 'string') {
-    return getValue(obj, key.split('.'))
+    return getValue(arg, key.split('.'))
   }
 
   if (key.length === 1) {
-    if (Object.prototype.hasOwnProperty.call(obj, key[0])) {
-      return obj[key[0]] !== undefined ? obj[key[0]] : null
+    if (Object.prototype.hasOwnProperty.call(arg, key[0])) {
+      return arg[key[0]] !== undefined ? arg[key[0]] : null
     } else {
       return null
     }
   }
 
-
-
-  if (Object.prototype.hasOwnProperty.call(obj, key[0])) {
-    return getValue(obj[key[0]], key.slice(1))
+  if (Object.prototype.hasOwnProperty.call(arg, key[0])) {
+    return getValue(arg[key[0]], key.slice(1))
   }
 }
 
---
+++ js/string-convert.js
@@ -0,0 +1,27 @@
+function toUpperCase(str) {
+  if (str[0]) {
+    return str.replace(str[0], str[0].toUpperCase())
+  } else {
+    return ''
+  }
+}
+
+function toLowerCase(str) {
+  if (str[0]) {
+    return str.replace(str[0], str[0].toLowerCase())
+  } else {
+    return ''
+  }
+}
+
+function toUnderscore(str) {
+  return str.replace(/([A-Z])/g, '_$1').toLowerCase()
+}
+
+function toCamelCase(str) {
+  return str.replace(/\_(\w)/g, function (all, letter) {
+    return letter.toUpperCase()
+  })
+}
+
+module.exports = { toUpperCase, toLowerCase, toUnderscore, toCamelCase }
